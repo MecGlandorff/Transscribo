a1e3d7116c6e1baeb42c682f78b97c9bdb4a69a3
MecGlandorff <mpjglandorff@gmail.com>
2024-12-10 15:30:17 +0200

    latest tweaks

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 
 
-# Transscribe
-
+# Main
 import os
 import sys
 from load_mp4 import extract_audio
@@ -11,13 +10,13 @@ from transcribe import transcribe_audio
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python main.py path_to_video.mp4")
+        print("usage: python main.py path_to_video.mp4")
         sys.exit(1)
 
     mp4_path = sys.argv[1]
 
     if not os.path.isfile(mp4_path):
-        print(f"Error: File {mp4_path} does not exist")
+        print(f"error: File {mp4_path} does not exist")
         sys.exit(1)
 
     audio_path = "extracted_audio.wav"
